2806a7aca79061494e91ef540b5795437fb540f0
Graham Clark <graham@vouchercloud.com>
2014-04-07 14:56:13 +0100

    controllers and scope

--- AngularPlayground/js/app.js
+++ AngularPlayground/js/app.js
@@ -1,4 +1,23 @@
 ﻿var app = angular.module("myApp", []);
-app.controller('MyController', function ($scope) {
-    $scope.name = 'Graham';
+app.controller('FirstController', function ($scope) {
+    $scope.counter = 0;
+
+    $scope.add = function (amount) {
+        $scope.counter += amount;
+    };
+
+    $scope.subtract = function(amount) {
+        $scope.counter -= amount;
+    };
+});
+
+app.controller('ParentController', function($scope) {
+    $scope.person = { greeted: false };
+});
+
+app.controller('ChildController', function($scope) {
+    $scope.sayHello = function() {
+        $scope.person.name = 'Graham';
+        $scope.person.greeted = true;
+    };
 });
