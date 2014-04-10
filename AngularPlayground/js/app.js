ddf00f2cd419540e468e5ab892b6adb0190759a6
Graham Clark <graham@vouchercloud.com>
2014-04-10 13:59:29 +0100

    directives and binding

--- AngularPlayground/js/app.js
+++ AngularPlayground/js/app.js
@@ -24,11 +24,22 @@ angular.module('myApp', ['emailParser', 'myApp.filters'])
     .controller('PlayController', [
         '$scope',
         '$filter',
-        function ($scope, $filter) {
+        function($scope, $filter) {
             $scope.$watch('inName', function(inName) {
                 if (inName) {
                     $scope.name = $filter('lowercase')(inName);
                 }
             });
         }
-    ]);
+    ])
+    .directive('myDirective', function() {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                myUrl: '=someAttr',
+                myLinkText: '@'
+            },
+            template: '<div><label>My Url Field:</label><input type="text" ng-model="myUrl"/><a href="{{myUrl}}">{{myLinkText}}</a></div>'
+        };
+    });
